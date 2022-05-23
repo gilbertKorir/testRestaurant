d6a1e4db3f42daec6652c3bff677d45f4b6b9f5c
gilbertKorir <jerikorir@gmail.com>
2022-05-23 23:31:21 +0300

    bind view added

--- app/src/main/java/com/example/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/myrestaurants/MainActivity.java
@@ -1,25 +1,37 @@
 package com.example.myrestaurants;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    private Button mFindRestaurantsButton;
+    @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mFindRestaurantsButton = (Button)findViewById(R.id.findRestaurantsButton);
-        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        ButterKnife.bind(this);
+
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //do something
+                String location = mLocationEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                intent.putExtra("location", location);
+                startActivity(intent);
             }
         });
     }
-
 }
