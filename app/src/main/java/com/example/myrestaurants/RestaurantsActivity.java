b65c97eeb6819b55e734301780ff84c0841102b1
gilbertKorir <jerikorir@gmail.com>
2022-05-30 12:20:05 +0300

    ft: added an api

--- app/src/main/java/com/example/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/example/myrestaurants/RestaurantsActivity.java
@@ -13,19 +13,18 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+//import okhttp3.Callback;
+import okhttp3.Response;
+import retrofit2.Call;
+import retrofit2.Callback;
 
 public class RestaurantsActivity extends AppCompatActivity {
     @BindView(R.id.locationTextView) TextView mLocationTextView;
     @BindView(R.id.listView) ListView mListView;
-    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro",
-            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
-            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
-            "Lardo", "Portland City Grill", "Fat Head's Brewery",
-            "Chipotle", "Subway"};
-    private String[] cuisines = new String[] {"Vegan Food", "Breakfast", "Fishs Dishs", "Scandinavian", "Coffee", "English Food", "Burgers", "Fast Food", "Noodle Soups", "Mexican", "BBQ", "Cuban", "Bar Food", "Sports Bar", "Breakfast", "Mexican" };
-//    public static final String TAG = RestaurantActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,26 +37,53 @@ public class RestaurantsActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         mLocationTextView.setText("Here are all the restaurants near: " + location);
-        MyRestaurantsArrayAdapter adapter = new MyRestaurantsArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants, cuisines); // the arguments must match constructor's parameters!
-        mListView.setAdapter(adapter);
 
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
 
                 String restaurant = ((TextView)view).getText().toString();
-                Log.v("RestaurantsActivity", "In the onItemClickListener!");
                 Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
 
+                YelpApi client = YelpClient.getClient();
+                Call<YelpBusinessesSearchResponse> call = client.getRestaurants(location, "restaurants");
+
+                call.enqueue(new Callback<YelpBusinessesSearchResponse>() {
+                    @Override
+                    public void onResponse(Call<YelpBusinessesSearchResponse> call, Response<YelpBusinessesSearchResponse> response) {
+                        if (response.isSuccessful()) {
+                            List<Business> restaurantsList = response.body().getBusinesses();
+                            String[] restaurants = new String[restaurantsList.size()];
+                            String[] categories = new String[restaurantsList.size()];
+
+                            for (int i = 0; i < restaurants.length; i++){
+                                restaurants[i] = restaurantsList.get(i).getName();
+                            }
+
+                            for (int i = 0; i < categories.length; i++) {
+                                Category category = restaurantsList.get(i).getCategories().get(0);
+                                categories[i] = category.getTitle();
+                            }
+
+                            ArrayAdapter adapter
+                                    = new MyRestaurantsArrayAdapter(RestaurantsActivity.this, android.R.layout.simple_list_item_1, restaurants, categories);
+                            mListView.setAdapter(adapter);
+
+                        }
+                    }
+
+                    @Override
+                    public void onResponse(Call<YelpBusinessesSearchResponse> call, retrofit2.Response<YelpBusinessesSearchResponse> response) {
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<YelpBusinessesSearchResponse> call, Throwable t) {
+
+                    }
+
+                });
             }
-
         });
-//
-//        Intent intent = getIntent();
-//        String location = intent.getStringExtra("location");
-//        mLocationTextView.setText("Here are all the restaurants near: " + location);
-//        Log.d(TAG, "In the onCreate method!");
-
-
     }
 }
