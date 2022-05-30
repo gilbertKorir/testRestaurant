66f3fdddab2096e2be5f271c2a662515f42d52fa
gilbertKorir <jerikorir@gmail.com>
2022-05-30 12:55:45 +0300

    ft: added a progress bar and an error log

--- app/src/main/java/com/example/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/example/myrestaurants/RestaurantsActivity.java
@@ -10,6 +10,7 @@ import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,6 +26,9 @@ import retrofit2.Callback;
 public class RestaurantsActivity extends AppCompatActivity {
     @BindView(R.id.locationTextView) TextView mLocationTextView;
     @BindView(R.id.listView) ListView mListView;
+    private static final String TAG = RestaurantsActivity.class.getSimpleName();
+    @BindView(R.id.errorTextView) TextView mErrorTextView;
+    @BindView(R.id.progressBar) ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,45 +49,65 @@ public class RestaurantsActivity extends AppCompatActivity {
                 String restaurant = ((TextView)view).getText().toString();
                 Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
 
-                YelpApi client = YelpClient.getClient();
-                Call<YelpBusinessesSearchResponse> call = client.getRestaurants(location, "restaurants");
-
-                call.enqueue(new Callback<YelpBusinessesSearchResponse>() {
-                    @Override
-                    public void onResponse(Call<YelpBusinessesSearchResponse> call, Response<YelpBusinessesSearchResponse> response) {
-                        if (response.isSuccessful()) {
-                            List<Business> restaurantsList = response.body().getBusinesses();
-                            String[] restaurants = new String[restaurantsList.size()];
-                            String[] categories = new String[restaurantsList.size()];
-
-                            for (int i = 0; i < restaurants.length; i++){
-                                restaurants[i] = restaurantsList.get(i).getName();
-                            }
-
-                            for (int i = 0; i < categories.length; i++) {
-                                Category category = restaurantsList.get(i).getCategories().get(0);
-                                categories[i] = category.getTitle();
-                            }
-
-                            ArrayAdapter adapter
-                                    = new MyRestaurantsArrayAdapter(RestaurantsActivity.this, android.R.layout.simple_list_item_1, restaurants, categories);
-                            mListView.setAdapter(adapter);
-
-                        }
-                    }
-
-                    @Override
-                    public void onResponse(Call<YelpBusinessesSearchResponse> call, retrofit2.Response<YelpBusinessesSearchResponse> response) {
-
-                    }
-
-                    @Override
-                    public void onFailure(Call<YelpBusinessesSearchResponse> call, Throwable t) {
-
-                    }
-
-                });
             }
         });
+
+        //callback method
+
+        YelpApi client = YelpClient.getClient();
+        Call<YelpBusinessesSearchResponse> call = client.getRestaurants(location, "restaurants");
+
+        call.enqueue(new Callback<YelpBusinessesSearchResponse>() {
+            @Override
+            public void onResponse(Call<YelpBusinessesSearchResponse> call, retrofit2.Response<YelpBusinessesSearchResponse> response) {
+                    if (response.isSuccessful()) {
+                        List<Business> restaurantsList = response.body().getBusinesses();
+                        String[] restaurants = new String[restaurantsList.size()];
+                        String[] categories = new String[restaurantsList.size()];
+
+                        for (int i = 0; i < restaurants.length; i++){
+                            restaurants[i] = restaurantsList.get(i).getName();
+                        }
+
+                        for (int i = 0; i < categories.length; i++) {
+                            Category category = restaurantsList.get(i).getCategories().get(0);
+                            categories[i] = category.getTitle();
+                        }
+                        ArrayAdapter adapter = new MyRestaurantsArrayAdapter(RestaurantsActivity.this, android.R.layout.simple_list_item_1, restaurants, categories);
+                        mListView.setAdapter(adapter);
+                        showRestaurants();
+                    } else {
+                        showUnsuccessfulMessage();
+                    }
+                }
+
+
+            @Override
+            public void onFailure(Call<YelpBusinessesSearchResponse> call, Throwable t) {
+                Log.e("Error Message", "onFailure: ",t );
+                hideProgressBar();
+                showFailureMessage();
+            }
+
+        });
+
+    }
+    private void showFailureMessage() {
+        mErrorTextView.setText("Something went wrong. Please check your Internet connection and try again later");
+        mErrorTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void showUnsuccessfulMessage() {
+        mErrorTextView.setText("Something went wrong. Please try again later");
+        mErrorTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void showRestaurants() {
+        mListView.setVisibility(View.VISIBLE);
+        mLocationTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgressBar() {
+        mProgressBar.setVisibility(View.GONE);
     }
 }
