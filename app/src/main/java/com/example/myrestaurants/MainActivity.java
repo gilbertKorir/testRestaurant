1990a26b53eb464e6884e0b43e7820e8529d4d77
gilbertKorir <jerikorir@gmail.com>
2022-06-08 21:05:20 +0300

    ft: adde a serach view

--- app/src/main/java/com/example/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/myrestaurants/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.myrestaurants.ui.SavedRestaurantListActivity;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,73 +27,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 //    private SharedPreferences mSharedPreferences;
 //    private SharedPreferences.Editor mEditor;
-private DatabaseReference mSearchedLocationReference;
-private ValueEventListener mSearchedLocationReferenceListener;
+//private DatabaseReference mSearchedLocationReference;
+//private ValueEventListener mSearchedLocationReferenceListener;
 
     @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
-    @BindView(R.id.locationEditText) EditText mLocationEditText;
     @BindView(R.id.appNameTextView) TextView mAppNameTextView;
+    @BindView(R.id.savedRestaurantsButton) Button mSavedRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        mSearchedLocationReference = FirebaseDatabase
-                .getInstance()
-                .getReference()
-                .child(Constants.FIREBASE_CHILD_SEARCHED_LOCATION);
-        mSearchedLocationReferenceListener = mSearchedLocationReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {//something changed!
-                //   code here is executed when Firebase recognizes a change made to the
-                //   node being listened to, and provides a new dataSnapshot.
-                for (DataSnapshot locationSnapshot : dataSnapshot.getChildren()) {
-                    String location = locationSnapshot.getValue().toString();
-                    Log.d("Locations updated", "location: " + location); //log
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            } //attach listener
-
-        });
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-//        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-//        mEditor = mSharedPreferences.edit();
 
         mFindRestaurantsButton.setOnClickListener(this);
+        mSavedRestaurantsButton.setOnClickListener(this);
     }
     @Override
     public void onClick(View v) {
         if (v == mFindRestaurantsButton) {
-            String location = mLocationEditText.getText().toString();
-            saveLocationToFirebase(location);
-
-//            if(!(location).equals("")) {
-//                addToSharedPreferences(location);
-//            }
             Intent intent = new Intent(MainActivity.this, RestaurantListActivity.class);
-            intent.putExtra("location", location);
             startActivity(intent);
         }
-    }
-    //add .push() to prevent location being overwritten
-    public void saveLocationToFirebase(String location) {
-        mSearchedLocationReference.push().setValue(location);
-    }
-//    private void addToSharedPreferences(String location) {
-//        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
-//    }
-
-    @Override
-    protected void onDestroy() {
-        //    defined in 'top level' of activity, not nested within another block.
-        //    code here is executed when the user quits the activity.
-        super.onDestroy();
-        mSearchedLocationReference.removeEventListener(mSearchedLocationReferenceListener);
+        if (v == mSavedRestaurantsButton) {
+            Intent intent = new Intent(MainActivity.this, SavedRestaurantListActivity.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/myrestaurants/RestaurantListActivity.java
+++ app/src/main/java/com/example/myrestaurants/RestaurantListActivity.java
@@ -9,8 +9,12 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import com.example.myrestaurants.adapters.RestaurantListAdapter;
@@ -33,8 +37,12 @@ public class RestaurantListActivity extends AppCompatActivity {
 //    @BindView(R.id.listView) ListView mListView;
 
     private static final String TAG = RestaurantListActivity.class.getSimpleName();
-//    private SharedPreferences mSharedPreferences;
-//    private String mRecentAddress;
+
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+    private String mRecentAddress;
+
+
     @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
     @BindView(R.id.errorTextView) TextView mErrorTextView;
     @BindView(R.id.progressBar) ProgressBar mProgressBar;
@@ -49,44 +57,82 @@ public class RestaurantListActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         Intent intent = getIntent();
-//        String location = intent.getStringExtra("location");
-//        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-//        mRecentAddress = mSharedPreferences.getString(Constants.PREFERENCES_LOCATION_KEY, null);
+        String location = intent.getStringExtra("location");
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mRecentAddress = mSharedPreferences.getString(Constants.PREFERENCES_LOCATION_KEY, null);
 
         // We are changing the **String location = intent.getStringExtra("location");**  location variable to use the value stored in the sharedPreferences  **String location = mRecentAddress**  as shown below
-        String location = intent.getStringExtra("location");
+//        String location = intent.getStringExtra("location");
 
-        YelpApi client = YelpClient.getClient();
-        Call<YelpBusinessesSearchResponse> call = client.getRestaurants(location, "restaurants");
+        if (mRecentAddress != null) {
+            YelpApi client = YelpClient.getClient();
+            Call<YelpBusinessesSearchResponse> call = client.getRestaurants(location, "restaurants");
 
-        call.enqueue(new Callback<YelpBusinessesSearchResponse>() {
-            @Override
-            public void onResponse(Call<YelpBusinessesSearchResponse> call, Response<YelpBusinessesSearchResponse> response) {
-                hideProgressBar();
+            call.enqueue(new Callback<YelpBusinessesSearchResponse>() {
+                @Override
+                public void onResponse(Call<YelpBusinessesSearchResponse> call, Response<YelpBusinessesSearchResponse> response) {
+                    hideProgressBar();
 
-                if (response.isSuccessful()) {
-                    restaurants = response.body().getBusinesses();
-                    mAdapter = new RestaurantListAdapter(RestaurantListActivity.this, restaurants);
-                    mRecyclerView.setAdapter(mAdapter);
+                    if (response.isSuccessful()) {
+                        restaurants = response.body().getBusinesses();
+                        mAdapter = new RestaurantListAdapter(RestaurantListActivity.this, restaurants);
+                        mRecyclerView.setAdapter(mAdapter);
 
-                    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(RestaurantListActivity.this);
-                    mRecyclerView.setLayoutManager(layoutManager);
-                    mRecyclerView.setHasFixedSize(true);
+                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(RestaurantListActivity.this);
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
 
-                    showRestaurants();
-                } else {
-                    showUnsuccessfulMessage();
+                        showRestaurants();
+                    } else {
+                        showUnsuccessfulMessage();
+                    }
                 }
-            }
 
+                @Override
+                public void onFailure(Call<YelpBusinessesSearchResponse> call, Throwable t) {
+                    hideProgressBar();
+                    showFailureMessage();
+                }
+            });
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        ButterKnife.bind(this);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onFailure(Call<YelpBusinessesSearchResponse> call, Throwable t) {
-                hideProgressBar();
-                showFailureMessage();
+            public boolean onQueryTextSubmit(String location) {
+                addToSharedPreferences(location);
+                addToSharedPreferences(location);
+                fetchRestaurants(location);
+                return false;
             }
-
+            @Override
+            public boolean onQueryTextChange(String location) {
+                return false;
+            }
         });
 
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void addToSharedPreferences(String location) {
+        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
     }
 
     private void showFailureMessage() {
@@ -105,5 +151,38 @@ public class RestaurantListActivity extends AppCompatActivity {
 
     private void hideProgressBar() {
         mProgressBar.setVisibility(View.GONE);
+    }
+
+    private void fetchRestaurants(String location){
+        YelpApi client = YelpClient.getClient();
+        Call<YelpBusinessesSearchResponse> call = client.getRestaurants(location, "restaurants");
+        call.enqueue(new Callback<YelpBusinessesSearchResponse>() {
+            @Override
+            public void onResponse(Call<YelpBusinessesSearchResponse> call, Response<YelpBusinessesSearchResponse> response) {
+
+                hideProgressBar();
+
+                if (response.isSuccessful()) {
+                    restaurants = response.body().getBusinesses();
+                    mAdapter = new RestaurantListAdapter(RestaurantListActivity.this, restaurants);
+                    mRecyclerView.setAdapter(mAdapter);
+                    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(RestaurantListActivity.this);
+                    mRecyclerView.setLayoutManager(layoutManager);
+                    mRecyclerView.setHasFixedSize(true);
+
+                    showRestaurants();
+                } else {
+                    showUnsuccessfulMessage();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<YelpBusinessesSearchResponse> call, Throwable t) {
+                Log.e(TAG, "onFailure: ",t );
+                hideProgressBar();
+                showFailureMessage();
+            }
+
+        });
     }
 }
