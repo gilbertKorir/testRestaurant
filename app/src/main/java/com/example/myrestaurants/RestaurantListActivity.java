0e1e2d0996fc389c42505dea250011b3a3bda0fc
gilbertKorir <jerikorir@gmail.com>
2022-06-08 22:46:06 +0300

    ft: adde a serach view

--- app/src/main/java/com/example/myrestaurants/RestaurantListActivity.java
+++ app/src/main/java/com/example/myrestaurants/RestaurantListActivity.java
@@ -1,21 +1,23 @@
 package com.example.myrestaurants;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+        import androidx.appcompat.app.AppCompatActivity;
+        import androidx.core.view.MenuItemCompat;
+        import androidx.recyclerview.widget.LinearLayoutManager;
+        import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ProgressBar;
-import android.widget.SearchView;
-import android.widget.TextView;
+        import android.content.Intent;
+        import android.content.SharedPreferences;
+        import android.os.Bundle;
+        import android.preference.PreferenceManager;
+        import android.util.Log;
+        import android.view.Menu;
+        import android.view.MenuInflater;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.ProgressBar;
+        import android.widget.SearchView;
+        import android.widget.TextView;
 
 import com.example.myrestaurants.adapters.RestaurantListAdapter;
 import com.example.myrestaurants.models.Business;
@@ -25,29 +27,22 @@ import com.example.myrestaurants.network.YelpClient;
 
 import java.util.List;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-//import okhttp3.Callback;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+        import butterknife.BindView;
+        import butterknife.ButterKnife;
+        import retrofit2.Call;
+        import retrofit2.Callback;
+        import retrofit2.Response;
 
 public class RestaurantListActivity extends AppCompatActivity {
-//    @BindView(R.id.locationTextView) TextView mLocationTextView;
-//    @BindView(R.id.listView) ListView mListView;
-
-    private static final String TAG = RestaurantListActivity.class.getSimpleName();
-
     private SharedPreferences mSharedPreferences;
     private SharedPreferences.Editor mEditor;
     private String mRecentAddress;
 
-
+    private static final String TAG = RestaurantListActivity.class.getSimpleName();
+    private RestaurantListAdapter mAdapter;
     @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
     @BindView(R.id.errorTextView) TextView mErrorTextView;
     @BindView(R.id.progressBar) ProgressBar mProgressBar;
-
-    private RestaurantListAdapter mAdapter;
     public List<Business> restaurants;
 
     @Override
@@ -56,48 +51,15 @@ public class RestaurantListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_restaurants);
         ButterKnife.bind(this);
 
-        Intent intent = getIntent();
-        String location = intent.getStringExtra("location");
         mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         mRecentAddress = mSharedPreferences.getString(Constants.PREFERENCES_LOCATION_KEY, null);
-
-        // We are changing the **String location = intent.getStringExtra("location");**  location variable to use the value stored in the sharedPreferences  **String location = mRecentAddress**  as shown below
-//        String location = intent.getStringExtra("location");
-
-        if (mRecentAddress != null) {
-            YelpApi client = YelpClient.getClient();
-            Call<YelpBusinessesSearchResponse> call = client.getRestaurants(location, "restaurants");
-
-            call.enqueue(new Callback<YelpBusinessesSearchResponse>() {
-                @Override
-                public void onResponse(Call<YelpBusinessesSearchResponse> call, Response<YelpBusinessesSearchResponse> response) {
-                    hideProgressBar();
-
-                    if (response.isSuccessful()) {
-                        restaurants = response.body().getBusinesses();
-                        mAdapter = new RestaurantListAdapter(RestaurantListActivity.this, restaurants);
-                        mRecyclerView.setAdapter(mAdapter);
-
-                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(RestaurantListActivity.this);
-                        mRecyclerView.setLayoutManager(layoutManager);
-                        mRecyclerView.setHasFixedSize(true);
-
-                        showRestaurants();
-                    } else {
-                        showUnsuccessfulMessage();
-                    }
-                }
-
-                @Override
-                public void onFailure(Call<YelpBusinessesSearchResponse> call, Throwable t) {
-                    hideProgressBar();
-                    showFailureMessage();
-                }
-            });
+        if(mRecentAddress != null){
+            fetchRestaurants(mRecentAddress);
         }
     }
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_search, menu);
         ButterKnife.bind(this);
@@ -108,15 +70,15 @@ public class RestaurantListActivity extends AppCompatActivity {
         MenuItem menuItem = menu.findItem(R.id.action_search);
         SearchView searchView = (SearchView) menuItem.getActionView();
 
-
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String location) {
                 addToSharedPreferences(location);
-                addToSharedPreferences(location);
                 fetchRestaurants(location);
                 return false;
             }
+
+
             @Override
             public boolean onQueryTextChange(String location) {
                 return false;
@@ -127,12 +89,8 @@ public class RestaurantListActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item){
         return super.onOptionsItemSelected(item);
-    }
-
-    private void addToSharedPreferences(String location) {
-        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
     }
 
     private void showFailureMessage() {
@@ -151,6 +109,10 @@ public class RestaurantListActivity extends AppCompatActivity {
 
     private void hideProgressBar() {
         mProgressBar.setVisibility(View.GONE);
+    }
+
+    private void addToSharedPreferences(String location) {
+        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
     }
 
     private void fetchRestaurants(String location){
@@ -186,3 +148,4 @@ public class RestaurantListActivity extends AppCompatActivity {
         });
     }
 }
+
